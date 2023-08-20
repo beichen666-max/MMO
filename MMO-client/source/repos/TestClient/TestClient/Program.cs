9188eb0d0a51a0314c364f2494c1a775e8d4dc1a
beichen666-max <1002617208@qq.com>
2023-08-21 00:41:34 +0800

    代码提交到22

---
+++ MMO-client/source/repos/TestClient/TestClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace TestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var host = "127.0.0.1";
+            int port = 32510;
+
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(host), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(iPEndPoint);
+
+            Console.WriteLine("成功连接到服务器");
+
+            string text = "大河之剑天上来";
+            // 将字符串转为字节数组
+            byte[] body = Encoding.UTF8.GetBytes(text);
+            SendMessage(socket,body);
+            SendMessage(socket, Encoding.UTF8.GetBytes("劝君更尽一杯酒"));
+            Console.ReadKey ();
+        }
+
+        //socket发送字节数组
+        static void SendMessage(Socket socket, byte[] body)
+        {
+            byte[] lenBytes = BitConverter.GetBytes(body.Length);
+            socket.Send(lenBytes);
+            socket.Send(body);
+        }
+    }
+}
--- source/repos/MMO-SERVER/Game-Server/Program.cs
+++ source/repos/MMO-SERVER/Game-Server/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Game_Server
+﻿using GameServer.Network;  // 使用Game-Sever下的Network
+using Network;
+using System.Net;
+using System.Net.Sockets;  // ?
+using System.Text;
+
+namespace Game_Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // 创建监听者对象
+            TcpSocketListener listener = new TcpSocketListener("0.0.0.0", 32510);
+            listener.SocketConnected += OnClientConnected;
+            listener.Start();
+            Console.ReadKey();
+
+        }
+
+        private static void OnClientConnected(object? sender, Socket socket)
+        {
+            // 接收到客户端
+            var ipEndPoint = socket.RemoteEndPoint as IPEndPoint; // 向下转型，类型还原
+            Console.WriteLine("有客户端连入，对方端口是：" + ipEndPoint.Port);
+
+            var lfd = new LengthFieldDecoder(socket, 64 * 1024, 0, 4, 0, 4);
+            lfd.DataReceived += OnDataReceived;
+            lfd.Start(); // 启动解码器
+        }
+
+        private static void OnDataReceived(byte[] buffer)
+        {
+            string txt = Encoding.UTF8.GetString(buffer);
+            Console.WriteLine(txt);
         }
     }
 }
